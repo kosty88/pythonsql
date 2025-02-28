01067a3c5632e2610bb7627792510751924a1ed9
Bobr <kostyvorobev24@gmail.com>
2025-02-28 15:48:29 +0300

    Second commit

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def add_client(conn, first_name, last_name, email, phones=None):
         cur.execute("""
         INSERT INTO client_phone (client_id, phones) VALUES (%s, %s);
                 """, (client_id, phones))
-        conn.commit()
         print(f"Клиент создан c ID {client_id}")
         return client_id
 
@@ -52,8 +51,6 @@ def add_phone(conn, client_id, phones):
             return False
         cur.execute("""INSERT INTO client_phone (client_id, phones) VALUES (%s, %s)
             """, (client_id, phones))
-
-        conn.commit()
         print("Телефон успешно добавлен")
         return True
 
@@ -62,7 +59,7 @@ def add_phone(conn, client_id, phones):
         return False
 
 
-def change_client(conn, id, last_name, first_name=None, email=None, phones=None):
+def change_client(conn, id, updates):
     cur = conn.cursor()
     try:
         cur.execute("""
@@ -71,14 +68,21 @@ def change_client(conn, id, last_name, first_name=None, email=None, phones=None)
         if not cur.fetchone():
             print("Ошибка: клиент с указанным ID не существует")
             return False
-        cur.execute("""
-                    UPDATE client SET last_name=%s WHERE id=%s;
-                    """, (last_name, id))
-        conn.commit()
-        print("Фамилия успешно изменена")
-        return True
+        listk = {}
+        for i in updates:
+            if not updates[i]:
+                continue
+            else:
+                listk[i] = updates[i]
+        for i in listk:
+            cur.execute(f"""
+            UPDATE client SET {i}=%s
+            WHERE id=%s;
+            """, (listk[i], id))
+            print(f"Данные {i} успешно изменены")
+            continue
 
-    except:
+    except :
         print(f"Ошибка: клиент с указанным ID  не существует")
         return False
 
@@ -92,7 +96,6 @@ def delete_phone(conn, id, phone):
         if cur.rowcount == 0:
             print("Ошибка: телефон не найден")
             return False
-        conn.commit()
         print("Телефон успешно удален")
         return True
 
@@ -118,49 +121,79 @@ def delete_client(conn, id):
                     """)
         print("Kлиент с указанным ID успешно удален")
         return True
-
     except:
         print(f"Ошибка: клиент с указанным ID не существует")
         return False
 
 
-def find_client(conn, first_name, last_name=None, email=None, phone=None):
-    cur = conn.cursor()
-    try:
-        cur.execute("""
-            SELECT id FROM client WHERE first_name = %s ;
-            """, (first_name, ))
-        if not cur.fetchone():
-            print("Ошибка: клиент с указанным именем не существует")
-            return False
-        cur.execute("""
-                    SELECT id, first_name, last_name, email FROM client WHERE first_name= %s;
-                    """, (first_name,))
-        print(cur.fetchone())
-        return True
+def find_client(conn, **kwargs):
+    valid_fields = ['first_name', 'last_name', 'email', 'phone']
+    conditions = []
+    params = []
+    join_phones = False
 
-    except:
-        print(f"Ошибка: клиент с указанным именем не существует")
-        return False
+    for field, value in kwargs.items():
+        if field in valid_fields and value is not None:
+            if field == 'phone':
+                join_phones = True
+                conditions.append("p.phones = %s")
+                params.append(value)
+            else:
+                conditions.append(f"c.{field} = %s")
+                params.append(value)
+
+    if not conditions:
+        return []
+
+    where_clause = " AND ".join(conditions)
+    if join_phones:
+        query = f'''
+                SELECT c.id, c.first_name, c.last_name, c.email
+                FROM client c
+                JOIN client_phone p ON c.id = p.client_id
+                WHERE {where_clause}
+            '''
+    else:
+        query = f'''
+                SELECT * FROM clients c
+                WHERE {where_clause}
+            '''
+    try:
+        cur = conn.cursor()
+        cur.execute(query, params)
+        return cur.fetchall()
+    except EnvironmentError as e:
+        print(e)
+        return []
 
 
 with psycopg2.connect(database="clients_db", user="postgres", password="PostgreS") as conn:
     create_tabl = create_db(conn)
 
-    # add_client1 = add_client(conn, "Иван","Помоев","ivan@mail.ru")
-    # add_client2 = add_client(conn, "Сергей","Чащин","sergey@mail.ru","891231231")
-    # add_client3 = add_client(conn, "Константин","Золотов","kosty@mail.ru","891732333")
-    # add_client4 = add_client(conn, "Аркадий","Эдуардов","arkasha@mail.ru","891923231")
-    # add_client5 = add_client(conn, "Алексей","Молчанов","lexa@mail.ru","891923231")
+    add_client1 = add_client(conn, "Иван","Помоев","ivan@mail.ru")
+    add_client2 = add_client(conn, "Сергей","Чащин","sergey@mail.ru","891231231")
+    add_client3 = add_client(conn, "Константин","Золотов","kosty@mail.ru","891732333")
+    add_client4 = add_client(conn, "Аркадий","Эдуардов","arkasha@mail.ru","891923231")
+    add_client5 = add_client(conn, "Алексей","Молчанов","lexa@mail.ru","891923231")
 
-    # add_phones = add_phone(conn,5, 8909123123)
+    if __name__ == "__main__":
 
-    # change_client1 = change_client(conn, 5, 'Аляскин')
+        add_phones = add_phone(conn,5, 8909123123)
 
-    # delete_phones = delete_phone(conn,5, 8909123123)
+        # change_client1 = change_client(conn, 4,{
+        #     "first_name": 'Эдуард',
+        #     "last_name": 'Райкин',
+        #     "email": ''
+        # })
 
-    # delete_clients = delete_client(conn,5)
+        # delete_phones = delete_phone(conn,5, 8909123123)
 
-    # find_clients = find_client(conn,'Иван')
+        # delete_clients = delete_client(conn,5)
+
+        # clients = find_client(conn, phone="8909123123",first_name='Алексей')
+        # print("Найденные клиенты:")
+        # for client in clients:
+        #     print(client)
 
 conn.close()
+
